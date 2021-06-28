a9e9a0399f3883f489e9ef7ae71c81b6ed9d1a2b
Georgelys Marcano <geogemar@gmail.com>
2021-06-27 21:05:50 -0400

    updates views

--- resources/lang/es/engineering.php
+++ resources/lang/es/engineering.php
@@ -1,4 +1,24 @@
 <?php
 return [
     'deaneng-section' => 'Decanato de Ingeniería',
+    'deaneng-author' => 'Andrés R. Pedroza R.',
+    'deaneng-author-title' => 'Decano de Ingeniería de Sistemas y Afines',
+    'deaneng-author-mail' => 'Decano de Ingeniería de Sistemas y Afines',
+    'deaneng-asist' => 'Decano de Ingeniería de Sistemas y Afines',
+    'deaneng-asist-title' => 'Decano de Ingeniería de Sistemas y Afines',
+    'deaneng-asist-title' => 'Decano de Ingeniería de Sistemas y Afines',
+    'deaneng-mission-title' => 'El Decanato de Ingeniería tiene como objetivos la formación de un profesional que responda a las necesidades de su entorno, con los conocimientos necesarios para analizar, diseñar, desarrollar, planificar, evaluar, controlar e innovar en las áreas de Tecnología de la Información . Preparando ingenieros para el trabajo en equipo, en especial de carácter interdisciplinario, en funciones de cooperación, liderazgo, capaces de utilizar los constantes cambios Tecnológicos para resolver problemas de diversa índole.Contando para ello con las instalaciones ambientalistas adecuadas para la educación, laboratorios actualizados y un excelente equipo de profesores, con conocimientos  adaptados a las actuales exigencias de un mercado ocupacional cada vez más competitivo, que requiere profesionales comprometidos con el desarrollo social.',
+    'deaneng-functions' => 'Decano de Ingeniería de Sistemas y Afines',
+    'deaneng-functions-1' => 'Decano de Ingeniería de Sistemas y Afines',
+    'deaneng-functions-2' => 'Decano de Ingeniería de Sistemas y Afines',
+    'deaneng-functions-3' => 'Decano de Ingeniería de Sistemas y Afines',
+    'deaneng-functions-4' => 'Decano de Ingeniería de Sistemas y Afines',
+    'deaneng-functions-5' => 'Decano de Ingeniería de Sistemas y Afines',
+    'deaneng-functions-6' => 'Decano de Ingeniería de Sistemas y Afines',
+    'deaneng-functions-7' => 'Decano de Ingeniería de Sistemas y Afines',
+    'deaneng-functions-8' => 'Decano de Ingeniería de Sistemas y Afines', 
+    'deaneng-functions-9' => 'Decano de Ingeniería de Sistemas y Afines',
+    'deaneng-functions-10' => 'Decano de Ingeniería de Sistemas y Afines',
+    'deaneng-functions-11' => 'Decano de Ingeniería de Sistemas y Afines',
+    
 ];
