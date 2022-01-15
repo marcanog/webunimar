b2f1ebdd13cd90e87f1ec601db5c4600673c57ba
GeorgelysM <geogemar@gmail.com>
2022-01-15 13:02:53 -0400

    update offerstudy view

--- resources/views/studyoffers.blade.php
+++ resources/views/studyoffers.blade.php
@@ -9,35 +9,45 @@
             <div class="d-flex justify-content-start">
                 <img id="img-section"
                      src="{{URL::asset('/image/logo-unimar.png')}}"><span
-                    style="font-size: 0.85em; width: 55%;">{{trans('vacademy.vacad-comcurric')}}</span>
+                    style="font-size: 0.85em; width: 55%;">{{trans('offers.o-title')}}</span>
             </div>
         </div>
-        {{--contents--}}
+        {{--contents offers pregrade--}}
         <div class="d-inline-flex justify-content-around m-4 content-vext">
-            {{--Vision--}}
+            {{--Enginering--}}
             <div class="links-interest" id="contentimg" style="background-color: #336699"
                  data-toggle="collapse" href="#collapse" role="button" aria-expanded="false"
                  aria-controls="collapse1">
                 <a id="campusbtn">
                     <img src="{{URL::asset('/image/secretary.png')}}">
-                    <span>{{trans('secretarygeneral.secretaryofic-title')}}</span>
+                    <span>{{trans('offers.o-enginerrs')}}</span>
                 </a>
             </div>
+            {{--humarte--}}
             <div class="links-interest" id="contentimg" style="background-color: #336699"
-                 data-toggle="collapse" href="#collapse3" role="button" aria-expanded="false"
+                 data-toggle="collapse" href="#collapse2" role="button" aria-expanded="false"
                  aria-controls="collapse2">
                 <a id="foundbtn">
                     <img src="{{URL::asset('/image/settings.svg')}}">
-                    <span>{{trans('secretarygeneral.secretaryofic-functions')}}</span>
+                    <span>{{trans('offers.o-enginerrs')}}</span>
                 </a>
             </div>
-            {{--units ads--}}
+            {{--ceys--}}
+            <div class="links-interest" id="contentimg" style="background-color: #336699"
+                 data-toggle="collapse" href="#collapse3" role="button" aria-expanded="false"
+                 aria-controls="collapse4">
+                <a id="campusbtn">
+                    <img src="{{URL::asset('/image/lecture.png')}}">
+                    <span>{{trans('offers.o-enginerrs')}}</span>
+                </a>
+            </div>
+            {{--cjyp--}}
             <div class="links-interest" id="contentimg" style="background-color: #336699"
                  data-toggle="collapse" href="#collapse4" role="button" aria-expanded="false"
                  aria-controls="collapse4">
                 <a id="campusbtn">
                     <img src="{{URL::asset('/image/lecture.png')}}">
-                    <span>{{trans('secretarygeneral.secretaryofic-depts')}}</span>
+                    <span>{{trans('offers.o-enginerrs')}}</span>
                 </a>
             </div>
         </div>
