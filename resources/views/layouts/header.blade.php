a176eee03e3f330c1fdde54aab4699844c3a8c4a
GeorgelysM <geogemar@gmail.com>
2022-01-20 12:10:04 -0400

    update url asset image

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,15 +6,15 @@
                 <div class="rss-bar">
                     <ul>
                         <a href="mailto:info@unimar.edu.ve"><img class="img-rrss"
-                                                                 src="{{URL::asset('./image/rrss/email.png')}}"></a>
+                                                                 src="{{URL::asset('image/rrss/email.png')}}"></a>
                         <a href="https://www.facebook.com/univ.demargarita"><img class="img-rrss"
-                                                                                 src="{{URL::asset('./image/rrss/facebook.png')}}"></a>
+                                                                                 src="{{URL::asset('image/rrss/facebook.png')}}"></a>
                         <a href="https://www.instagram.com/universidademargarita"><img class="img-rrss"
-                                                                                       src="{{URL::asset('./image/rrss/instagram.png')}}"></a>
+                                                                                       src="{{URL::asset('image/rrss/instagram.png')}}"></a>
                         <a href="https://www.twitter.com/somosunimar"><img class="img-rrss"
-                                                                           src="{{URL::asset('./image/rrss/gorjeo.png')}}"></a>
+                                                                           src="{{URL::asset('image/rrss/gorjeo.png')}}"></a>
                         <a href="https://www.linkedin.com/company/univdemargarita"><img class="img-rrss"
-                                                                                        src="{{URL::asset('./image/rrss/linkedin.png')}}"></a>
+                                                                                        src="{{URL::asset('image/rrss/linkedin.png')}}"></a>
                     </ul>
                 </div>
                 <nav class="navbar navbar-light bg-light search-bar">
@@ -22,25 +22,25 @@
                         <input class="search form-control" type="search" placeholder="{{trans('header.input_search')}}"
                                arial-label="search">
                         <img class="search-img btn btn-outline-success" id="btnsearch"
-                             src="{{ URL::asset('./image/lupa_b.png')}}" href="#">
+                             src="{{ URL::asset('image/lupa_b.png')}}" href="#">
                     </form>
                 </nav>
                 <div class="navbar language-icon">
                     <div class="nav-item dropdown show" id="languages">
                         <a class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
                            aria-haspopup="true" aria-expanded="false">
-                            <img src="{{URL::asset('./image/idioma.png')}}" style="width:30px;">
+                            <img src="{{URL::asset('image/idioma.png')}}" style="width:30px;">
                         </a>
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                             <a class="dropdown-item" href="{{URL('lang/es')}}"><img class="rounded-circle"
-                                                                                    src="{{URL::asset('./image/flags/flag-spain.png')}}"
+                                                                                    src="{{URL::asset('image/flags/flag-spain.png')}}"
                                                                                     style="width:25px;">{{trans('home.lang-es')}}
                             </a>
                             <a class="dropdown-item" href="{{URL('lang/en')}}"><img class="rounded-circle"
-                                                                                    src="{{URL::asset('./image/flags/flag-usa.png')}}"
+                                                                                    src="{{URL::asset('image/flags/flag-usa.png')}}"
                                                                                     style="width:25px;">{{trans('home.lang-en')}}
                             </a>
-                            {{--<a class="dropdown-item" href="lang/ita"><img class="rounded-circle" src="{{URL::asset('./image/flags/flag-italy.png')}}" style="width:25px;">{{trans('home.lang-it')}}</a>--}}
+                            {{--<a class="dropdown-item" href="lang/ita"><img class="rounded-circle" src="{{URL::asset('image/flags/flag-italy.png')}}" style="width:25px;">{{trans('home.lang-it')}}</a>--}}
                         </div>
                     </div>
                 </div>
@@ -49,7 +49,7 @@
                         @auth
                             <a href="{{ url('/admin') }}">{{ auth()->user()->name }}</a>
                         @else
-                            <a href="{{ route('login') }}"><img src="{{URL::asset('./image/login.png')}}"
+                            <a href="{{ route('login') }}"><img src="{{URL::asset('image/login.png')}}"
                                                                 style="width: 25px;">{{trans('header.login')}}</a>
                         @endauth
                     </div>
@@ -59,7 +59,7 @@
         {{-- Nav bar --}}
         <div class="navbar navbar-expand-lg navbar-light bg-light navbar-text">
             <div class="content-fluid d-flex logo me-4">
-                <a href="{{url('/home')}}"><img class="img-logo" src="{{URL::asset ('./image/logo-web-02.png') }}"></a>
+                <a href="{{url('/home')}}"><img class="img-logo" src="{{URL::asset ('image/logo-web-02.png') }}"></a>
             </div>
             <div class="d-flex nav-text">
                 <ul class="navbar navbar-light">
