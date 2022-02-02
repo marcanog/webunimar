9dfd7bfe7a1716c452178dba88d65a539613848b
GeorgelysM <geogemar@gmail.com>
2022-02-02 15:49:01 -0400

    update home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -47,47 +47,62 @@
             </div>
 
             <div class="d-flex justify-content-around m-4">
-            <div class="card m-2" style="width: 18rem;height: 25rem;">
-                    <img src="{{asset('image/newsimg/unimar3.jpg')}}" class="card-img-top" alt="image-news">
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold">Universidad de Margarita firma Convenio Interinstitucional con el Colegio de Contadores Públicos del estado Nueva Esparta</h5>
-                            <div class="text-justify m-0" id="card-content" style="font-size: small;">
-                                <p>El pasado 20 de octubre la Universidad de Margarita, UNIMAR , y el Colegio de Contadores Públicos de Nueva Esparta, CCPENE, firmaron un Convenio de cooperación interinstitucional, con el objetivo de desarrollar actividades conjuntas encaminadas a la formación, capacitación y divulgación de conocimientos de contaduría pública.</p>
+                <div class="card-group owl-carousel" id="newsdesktop">
+                    @foreach($news as $new)
+                        <div class="card m-2 h-100 responsive">
+                            <img src="{{asset('image/newsimg/unimar3.jpg')}}" class="card-img-top" alt="image-news">
+                            <div class="card-body">
+                                <h5 class="card-title font-weight-bold">{{($new->title)}}</h5>
+                                <div class="text-justify m-0" id="card-content" style="font-size: small;">
+                                    {!! ($new->resume) !!}
+                                </div>
+                                <a href="{{url('/newsunimar')}}" class="card-link" style="font-size: small;">{{trans('home.more')}}</a>
                             </div>
-                        <a href="{{url('/newsunimar')}}" class="card-link" style="font-size: small;">{{trans('home.more')}}</a>
-                    </div>
-                </div>
-                <div class="card m-2" style="width: 18rem;height: 25rem;">
-                    <img src="{{asset('image/newsimg/unimar3.jpg')}}" class="card-img-top" alt="image-news">
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold">Universidad de Margarita firma Convenio Interinstitucional con el Colegio de Contadores Públicos del estado Nueva Esparta</h5>
-                            <div class="text-justify m-0" id="card-content" style="font-size: small;">
-                                <p>El pasado 20 de octubre la Universidad de Margarita, UNIMAR , y el Colegio de Contadores Públicos de Nueva Esparta, CCPENE, firmaron un Convenio de cooperación interinstitucional, con el objetivo de desarrollar actividades conjuntas encaminadas a la formación, capacitación y divulgación de conocimientos de contaduría pública.</p>
-                            </div>
-                        <a href="{{url('/newsunimar')}}" class="card-link" style="font-size: small;">{{trans('home.more')}}</a>
-                    </div>
-                </div>
-                <div class="card m-2" style="width: 18rem;height: 25rem;">
-                    <img src="{{asset('image/newsimg/unimar3.jpg')}}" class="card-img-top" alt="image-news">
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold">Universidad de Margarita firma Convenio Interinstitucional con el Colegio de Contadores Públicos del estado Nueva Esparta</h5>
-                            <div class="text-justify m-0" id="card-content" style="font-size: small;">
-                                <p>El pasado 20 de octubre la Universidad de Margarita, UNIMAR , y el Colegio de Contadores Públicos de Nueva Esparta, CCPENE, firmaron un Convenio de cooperación interinstitucional, con el objetivo de desarrollar actividades conjuntas encaminadas a la formación, capacitación y divulgación de conocimientos de contaduría pública.</p>
-                            </div>
-                        <a href="{{url('/newsunimar')}}" class="card-link" style="font-size: small;">{{trans('home.more')}}</a>
-                    </div>
-                </div>
-                <div class="card m-2" style="width: 18rem;height: 25rem;">
-                    <img src="{{asset('image/newsimg/unimar3.jpg')}}" class="card-img-top" alt="image-news">
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold">Universidad de Margarita firma Convenio Interinstitucional con el Colegio de Contadores Públicos del estado Nueva Esparta</h5>
-                            <div class="text-justify m-0" id="card-content" style="font-size: small;">
-                                <p>El pasado 20 de octubre la Universidad de Margarita, UNIMAR , y el Colegio de Contadores Públicos de Nueva Esparta, CCPENE, firmaron un Convenio de cooperación interinstitucional, con el objetivo de desarrollar actividades conjuntas encaminadas a la formación, capacitación y divulgación de conocimientos de contaduría pública.</p>
-                            </div>
-                        <a href="{{url('/newsunimar')}}" class="card-link" style="font-size: small;">{{trans('home.more')}}</a>
-                    </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
+            <!-- <div class="card m-2" style="width: 18rem;height: 25rem;">
+                    <img src="{{asset('image/newsimg/unimar3.jpg')}}" class="card-img-top" alt="image-news">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">Universidad de Margarita firma Convenio Interinstitucional con el Colegio de Contadores Públicos del estado Nueva Esparta</h5>
+                            <div class="text-justify m-0" id="card-content" style="font-size: small;">
+                                <p>El pasado 20 de octubre la Universidad de Margarita, UNIMAR , y el Colegio de Contadores Públicos de Nueva Esparta, CCPENE, firmaron un Convenio de cooperación interinstitucional, con el objetivo de desarrollar actividades conjuntas encaminadas a la formación, capacitación y divulgación de conocimientos de contaduría pública.</p>
+                            </div>
+                        <a href="{{url('/newsunimar')}}" class="card-link" style="font-size: small;">{{trans('home.more')}}</a>
+                    </div>
+                </div>
+                <div class="card m-2" style="width: 18rem;height: 25rem;">
+                    <img src="{{asset('image/newsimg/unimar3.jpg')}}" class="card-img-top" alt="image-news">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">Universidad de Margarita firma Convenio Interinstitucional con el Colegio de Contadores Públicos del estado Nueva Esparta</h5>
+                            <div class="text-justify m-0" id="card-content" style="font-size: small;">
+                                <p>El pasado 20 de octubre la Universidad de Margarita, UNIMAR , y el Colegio de Contadores Públicos de Nueva Esparta, CCPENE, firmaron un Convenio de cooperación interinstitucional, con el objetivo de desarrollar actividades conjuntas encaminadas a la formación, capacitación y divulgación de conocimientos de contaduría pública.</p>
+                            </div>
+                        <a href="{{url('/newsunimar')}}" class="card-link" style="font-size: small;">{{trans('home.more')}}</a>
+                    </div>
+                </div>
+                <div class="card m-2" style="width: 18rem;height: 25rem;">
+                    <img src="{{asset('image/newsimg/unimar3.jpg')}}" class="card-img-top" alt="image-news">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">Universidad de Margarita firma Convenio Interinstitucional con el Colegio de Contadores Públicos del estado Nueva Esparta</h5>
+                            <div class="text-justify m-0" id="card-content" style="font-size: small;">
+                                <p>El pasado 20 de octubre la Universidad de Margarita, UNIMAR , y el Colegio de Contadores Públicos de Nueva Esparta, CCPENE, firmaron un Convenio de cooperación interinstitucional, con el objetivo de desarrollar actividades conjuntas encaminadas a la formación, capacitación y divulgación de conocimientos de contaduría pública.</p>
+                            </div>
+                        <a href="{{url('/newsunimar')}}" class="card-link" style="font-size: small;">{{trans('home.more')}}</a>
+                    </div>
+                </div>
+                <div class="card m-2" style="width: 18rem;height: 25rem;">
+                    <img src="{{asset('image/newsimg/unimar3.jpg')}}" class="card-img-top" alt="image-news">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">Universidad de Margarita firma Convenio Interinstitucional con el Colegio de Contadores Públicos del estado Nueva Esparta</h5>
+                            <div class="text-justify m-0" id="card-content" style="font-size: small;">
+                                <p>El pasado 20 de octubre la Universidad de Margarita, UNIMAR , y el Colegio de Contadores Públicos de Nueva Esparta, CCPENE, firmaron un Convenio de cooperación interinstitucional, con el objetivo de desarrollar actividades conjuntas encaminadas a la formación, capacitación y divulgación de conocimientos de contaduría pública.</p>
+                            </div>
+                        <a href="{{url('/newsunimar')}}" class="card-link" style="font-size: small;">{{trans('home.more')}}</a>
+                    </div>
+                </div>
+            </div> -->
 
             <div class="section-content">
                 <div class="d-flex justify-content-start">
@@ -95,7 +110,7 @@
                          src="{{URL::asset('image/calendar.png')}}"><span>{{trans('home.events')}}</span>
                 </div>
             </div>
-            <div class="content-event">
+            <!-- <div class="content-event">
                 <div class="card mx-3 mt-4 h-100">
                     <div class="row no-gutters">
                         <div class="col-md-4 text-white content-date">
@@ -152,6 +167,24 @@
                         </div>
                     </div>
                 </div>
+            </div> -->
+            <div class="card-group owl-carousel" id="eventdesktop">
+                @foreach($events as $event)
+                    <div class="card mx-3 mt-4 h-100">
+                        <div class="row no-gutters">
+                            <div class="col-md-4 text-white content-date">
+                                <p>{{ \Carbon\Carbon::parse(strtotime($event->start))->formatLocalized("%d %b")}}</p>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title font-weight-bold text-left">{{($event->title)}}</h5>
+                                    <p class="card-text text-justify">T {{($event->description)}}</p>
+                                    <a href="{{url('/eventsunimar')}}" class="card-link" style="font-size: small;">{{trans('home.more')}}</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
            
             <div class="section-content">
