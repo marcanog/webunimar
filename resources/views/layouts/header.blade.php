2d7d764e6c9caf2526743e73028a73edb8290f92
GeorgelysM <geogemar@gmail.com>
2022-02-04 14:21:44 -0400

    update header and footer media queries

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,8 @@
+{{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">--}}
+
+{{--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>--}}
+{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>--}}
+{{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>--}}
 <div class="top-bar">
     <div class="container-fluid mt-2">
         <div class="row">
@@ -54,16 +59,80 @@
             </div>
         </div>
     </div>
-    <nav class="navbar navbar-expand-md navbar-light bg-white">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{url('/home')}}">
-                <img class="logo horizontal-logo" src="{{asset('image/logo-web-02.png')}}" alt="UNIMAR logo">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+{{--    <nav class="navbar navbar-expand-md navbar-light bg-white">--}}
+{{--        <div class="container-fluid">--}}
+{{--            <a class="navbar-brand" href="{{url('/home')}}">--}}
+{{--                <img class="logo horizontal-logo" src="{{asset('image/logo-web-02.png')}}" alt="UNIMAR logo">--}}
+{{--            </a>--}}
+{{--            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">--}}
+{{--                <span class="navbar-toggler-icon"></span>--}}
+{{--            </button>--}}
+{{--            <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
+{{--                <ul class="navbar-nav ml-auto">--}}
+{{--                    <li class="nav-item dropdown">--}}
+{{--                        <a class="nav-link dropdown-toggle" href="" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+{{--                            {{trans('header.ourinstitut')}}--}}
+{{--                        </a>--}}
+{{--                        <div class="dropdown-menu" aria-labelledby="navbarDropdown2">--}}
+{{--                            <a class="dropdown-item" href="{{url('/ourinstitution')}}">{{trans('header.unimar')}}</a>--}}
+{{--                            <a class="dropdown-item" href="{{url('/organization')}}">{{trans('header.organization')}}</a>--}}
+{{--                            <a class="dropdown-item" href="#">{{trans('header.rectorade')}}</a>--}}
+{{--                            <a class="dropdown-item" href="#">{{trans('home.h-regulations')}}</a>--}}
+{{--                        </div>--}}
+{{--                    </li>--}}
+{{--                    <li class="nav-item dropdown">--}}
+{{--                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+{{--                            {{trans('header.academics')}}--}}
+{{--                        </a>--}}
+{{--                        <div class="dropdown-menu" aria-labelledby="navbarDropdown3">--}}
+{{--                            <a class="dropdown-item" href="{{url('/vacademic')}}">{{trans('header.vicerectorade')}}</a>--}}
+{{--                            <a class="dropdown-item" href="{{url('/deans')}}">{{trans('header.deanchip')}}</a>--}}
+{{--                        </div>--}}
+{{--                    </li>--}}
+{{--                    <li class="nav-item dropdown">--}}
+{{--                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+{{--                            {{trans('header.extent')}}--}}
+{{--                        </a>--}}
+{{--                        <div class="dropdown-menu" aria-labelledby="navbarDropdown3">--}}
+{{--                            <a class="dropdown-item" href="{{url('/vextension')}}">{{trans('header.vicerectorade')}}</a>--}}
+{{--                            <a class="dropdown-item" href="#">Decanatos</a>--}}
+{{--                        </div>--}}
+{{--                    </li>--}}
+{{--                    <li class="nav-item dropdown">--}}
+{{--                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+{{--                            {{trans('header.student')}}--}}
+{{--                        </a>--}}
+{{--                        <div class="dropdown-menu" aria-labelledby="navbarDropdown3">--}}
+{{--                            <a class="dropdown-item" href="{{url('/newstudents')}}">{{trans('header.new_additions')}}</a>--}}
+{{--                            <a class="dropdown-item" href="{{url('/regstudents')}}">{{trans('header.regstudents')}}</a>--}}
+{{--                            <a class="dropdown-item" href="{{url('/alumni')}}">{{trans('header.graduates')}}</a>--}}
+{{--                        </div>--}}
+{{--                    </li>--}}
+{{--                    <li class="nav-item">--}}
+{{--                        <a class="nav-link" href="{{url('/postgrade')}}">{{trans('header.postgrade')}}</a>--}}
+{{--                    </li>--}}
+{{--                    <li class="nav-item">--}}
+{{--                        <a class="nav-link" href="{{url('/dptoinvest')}}">{{trans('header.investigation')}}</a>--}}
+{{--                    </li>--}}
+{{--                    <li class="nav-item">--}}
+{{--                        <a class="nav-link" href="{{url('/services')}}">{{trans('header.services')}}</a>--}}
+{{--                    </li>--}}
+{{--                </ul>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </nav>--}}
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-md navbar-light bg-white">
+            <a class="navbar-brand text-dark" href="{{url('/home')}}" ><img class="logo horizontal-logo" src="{{asset('image/logo-web-02.png')}}" alt="UNIMAR logo"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#demo-navbar" aria-controls="demo-navbar" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
+
+            <div class="collapse navbar-collapse" id="demo-navbar">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link text-dark" href="{{url('/home')}}">Inicio <span class="sr-only">(current)</span></a>
+                    </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             {{trans('header.ourinstitut')}}
@@ -90,7 +159,7 @@
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown3">
                             <a class="dropdown-item" href="{{url('/vextension')}}">{{trans('header.vicerectorade')}}</a>
-{{--                            <a class="dropdown-item" href="#">Decanatos</a>--}}
+                            <a class="dropdown-item" href="#">Decanatos</a>
                         </div>
                     </li>
                     <li class="nav-item dropdown">
@@ -113,8 +182,12 @@
                         <a class="nav-link" href="{{url('/services')}}">{{trans('header.services')}}</a>
                     </li>
                 </ul>
+{{--                <form class="form-inline my-2 my-lg-0">--}}
+{{--                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">--}}
+{{--                    <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>--}}
+{{--                </form>--}}
             </div>
-        </div>
-    </nav>
+        </nav>
+    </div>
 </div>
 
