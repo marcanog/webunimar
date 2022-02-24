5a6f68dddfdfc666d7d6b8ee7de9de3e8992604a
GeorgelysM <geogemar@gmail.com>
2022-02-24 02:55:22 -0400

    new view graduation

---
+++ resources/views/graduation.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.layout')
+
+@section ('content')
+    <div class=".xl-container">
+        <div class="banner-main">
+            <img src="{{URL::asset('/image/banners/es/regstudents.jpg')}}">
+        </div>
+        {{--Section info--}}
+        <div class="section-content">
+            <div class="d-flex justify-content-start">
+                <img id="img-section"
+                     src="{{URL::asset('/image/logo-unimar.png')}}"><span>{{trans('regstudents.regstudent-title')}}</span>
+            </div>
+        </div>
+        {{--contents--}}
+    </div>
+@endsection
