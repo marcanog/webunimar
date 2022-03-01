770078e83dc0b3b68507582f8067e98c66d011de
GeorgelysM <geogemar@gmail.com>
2022-03-01 19:35:34 -0400

    add URLGenerator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
-// use Illuminate\Routing\UrlGenerator;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
