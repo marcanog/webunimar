9bd8254bfedcc0c4d47871cd8fbcd0c64ee19fe9
GeorgelysM <geogemar@gmail.com>
2022-03-01 19:39:55 -0400

    update serviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
+// use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
+        // if(config('app.env') === 'production') {
+        //     URL::forceScheme('https');
+        // }
     }
 }
