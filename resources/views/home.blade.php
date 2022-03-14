cf72bad6b32efa60af33dc4a53b6a0f9c1367874
GeorgelysM <geogemar@gmail.com>
2022-03-14 16:02:22 -0400

    update font-size news and event home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,8 +36,8 @@
                 <div class="card m-2 h-100 responsive">
                     <img src="{{asset('image/newsimg/unimar3.jpg')}}" class="card-img-top" alt="image-news">
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold">{{($new->title)}}</h5>
-                        <div class="text-justify m-0" id="card-content" style="font-size: small;">
+                        <h6 class="card-title font-weight-bold">{{($new->title)}}</h6>
+                        <div class="text-justify m-0" id="card-content" style="font-size: x-small;">
                             {!!($new->resume)!!}
                         </div>
                         <a href="{{url('/newsunimar')}}" class="card-link" style="font-size: small;">{{trans('home.more')}}</a>
