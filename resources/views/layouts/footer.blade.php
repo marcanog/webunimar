348f88311756284ff3a6159d3ac913f129bd7f73
GeorgelysM <geogemar@gmail.com>
2022-03-14 22:58:39 -0400

    update footer view

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -13,7 +13,7 @@
                         </a>
                     </li>
                     <li class="footer-item">
-                        <a class="footer-title" href="{{ URL('/rectorade') }}">{{trans('footer.f_rectors_office')}}</a>
+                        <a class="footer-title" href="#">{{trans('footer.f_rectors_office')}}</a>
                     </li>
                     <li class="footer-item">
                         <a class="footer-title"
@@ -83,11 +83,11 @@
                             {{trans('footer.f_elearning')}}
                         </a>
                     </li>
-                    <!-- <li class="footer-item">
-                        <a class="footer-title" href="{{url('/')}}">
-                            {{trans('footer.f_helps')}}
-                        </a>
-                    </li> -->
+{{--                    <li class="footer-item">--}}
+{{--                        <a class="footer-title" href="{{url('/')}}">--}}
+{{--                            {{trans('footer.f_helps')}}--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
                     <li class="footer-item">
                         <a class="footer-title" href="https://twitter.com/uniradio_unimar?s=20" target="_blank">
                             {{trans('footer.f_uniradio')}}
@@ -317,185 +317,6 @@
                         <span class="fa-sm-x2" style="float: right;">
                             <i class="fas fa-angle-right"></i>
                         </span>
-                    </button>
-                </h2>
-            </div>
-            <div id="collapseFour" class="collapse text-white" aria-labelledby="headingFour" data-parent="#accordionmobile">
-                <div class="content">
-                    <ul>
-                        <li class="footer-item">
-                            <a class="footer-title" href="{{url('/contactus')}}">{{trans('footer.f_dir_telf')}}</a>
-                        </li>
-                        <li class="footer-item">
-                            <a class="footer-title" href="{{url('/calendaracademy')}}">
-                                {{trans('footer.f_calendar')}}
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="content-fluid">
-            <div class="card-header infou">
-                <img src={{URL::asset('./image/logo-blanco.png')}}>
-                <span>{{trans('footer.f_location')}}</span>
-            </div>
-        </div>
-        <div class="content-fluid">
-            <div class="card-header text-center">
-                <span>{{trans('footer.f-copyrigth')}}</span>
-            </div>
-        </div>
-    </div>
-        <div class="content-fluid" style="background-color: #FFFFFF;">
-            <div class="card-header">
-                <div class="rrss">
-                    <ul class="d-flex justify-content-around list-unstyled">
-                        <li class="nav-item">
-                            <a href="mailto:info@unimar.edu.ve">
-                                <!-- <a onclick="location.href='mailto:info@unimar.edu.ve'"> -->
-                                <!-- <a onclick="sendmail()"> -->
-                                <img class="w-6 h-6" src={{URL::asset('/image/rrss/email.png')}}>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <!-- <a onclick="rrss()"> -->
-                            <a href="https://www.facebook.com/univ.demargarita">
-                                <img class="w-6 h-6" src={{URL::asset('/image/rrss/facebook.png')}}>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <!-- <a onclick="rrss()"> -->
-                            <a href="https://www.twitter.com/somosunimar">
-                                <img class="w-6 h-6" src={{URL::asset('/image/rrss/gorjeo.png')}}>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <!-- <a onclick="rrss()"> -->
-                            <a href="https://www.instagram.com/universidademargarita">
-                                <img class="w-6 h-6" src={{URL::asset('/image/rrss/instagram.png')}}>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <!-- <a onclick="rrss()"> -->
-                            <a href="https://www.linkedin.com/company/univdemargarita">
-                                <img class="w-6 h-6" src={{URL::asset('/image/rrss/linkedin.png')}}>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="content-fluid">
-            <div class="card-header" id="headingOne">
-                <h2 class="mb-0">
-                    <button class="btn btn-link btn-block text-white text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                        {{trans('footer.f_organization')}}
-                    </button>
-                </h2>
-            </div>
-
-            <div id="collapseOne" class="collapse text-white" aria-labelledby="headingOne" data-parent="#accordionmobile">
-                <div class="content">
-                   <ul>
-                       <li class="footer-item">
-                           <a class="footer-title" href="{{url('/rectorade')}}">{{trans('footer.f_rectors_office')}}</a>
-                       </li>
-                       <li class="footer-item">
-                           <a class="footer-title"
-                              href="{{ URL('/vicerectorades') }}">{{trans('footer.f_vice_chancellor')}}</a>
-                       </li>
-                       <li class="footer-item">
-                           <a class="footer-title" href="{{ URL('/deans') }}">{{trans('footer.f_deanchip')}}</a>
-                       </li>
-                       <li class="footer-item">
-                           <a class="footer-title"
-                              href="{{ URL('/healthystudent') }}">{{trans('footer.f_student_welfare')}}</a>
-                       </li>
-                   </ul>
-                </div>
-            </div>
-        </div>
-        <div class="content-fluid">
-            <div class="card-header" id="headingTwo">
-                <h2 class="mb-0">
-                    <button class="btn btn-link btn-block text-white text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        {{trans('footer.f_study_offers')}}
-                    </button>
-                </h2>
-            </div>
-            <div id="collapseTwo" class="collapse text-white" aria-labelledby="headingTwo" data-parent="#accordionmobile">
-                <div class="content">
-                    <ul>
-                        <li class="footer-item">
-                            <a class="footer-title" href="{{url('/pregrade')}}">
-                                {{trans('footer.f_undergraduate')}}
-                            </a>
-                        </li>
-                        <li class="footer-item">
-                            <a class="footer-title" href="{{url('/postgrade')}}">
-                                {{trans('footer.f_postgraduate')}}
-                            </a>
-                        </li>
-                        <li class="footer-item">
-                            <a class="footer-title" href="#">
-                                {{trans('footer.f_diplomas_courses')}}
-                            </a>
-                        </li>
-                        <li class="footer-item">
-                            <a class="footer-title" href="#">
-                                {{trans('footer.f_courses')}}
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="content-fluid">
-            <div class="card-header" id="headingThree">
-                <h2 class="mb-0">
-                    <button class="btn btn-link btn-block text-white text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        {{trans('footer.f_web_services')}}
-                    </button>
-                </h2>
-            </div>
-            <div id="collapseThree" class="collapse text-white" aria-labelledby="headingThree" data-parent="#accordionmobile">
-                <div class="content">
-                    <ul>
-                        <li class="footer-item">
-                            <a class="footer-title " href="{{url('/payments')}}">
-                                {{trans('footer.f_servacad')}}
-                            </a>
-                        </li>
-                        <li class="footer-item">
-                            <a class="footer-title " href="#">
-                                {{trans('footer.f_servadmin')}}
-                            </a>
-                        </li>
-                        <li class="footer-item">
-                            <a class="footer-title " href="https://www.unimarcientifica.edu.ve/adminmoodle/">
-                                {{trans('footer.f_elearning')}}
-                            </a>
-                        </li>
-                    <!-- <li class="footer-item">
-                        <a class="footer-title" href="{{url('/')}}">
-                            {{trans('footer.f_helps')}}
-                        </a>
-                    </li> -->
-                        <li class="footer-item">
-                            <a class="footer-title" href="https://twitter.com/uniradio_unimar?s=20" target="_blank">
-                                {{trans('footer.f_uniradio')}}
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="content-fluid">
-            <div class="card-header" id="headingFour">
-                <h2 class="mb-0">
-                    <button class="btn btn-link btn-block text-white text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                        {{trans('footer.f_quick_links')}}
                     </button>
                 </h2>
             </div>
