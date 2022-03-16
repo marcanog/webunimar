e12dd0ced010101ddde1e8a113f0e4d563008640
GeorgelysM <geogemar@gmail.com>
2022-03-16 05:10:14 -0400

    update ceys dean

--- resources/views/deancssoc.blade.php
+++ resources/views/deancssoc.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.layout')
 
 @section ('content')
-    <div class=".xl-container">
+    <div class="content">
         <div class="banner-main">
-            <img src="{{URL::asset('./image/nuestrosdecanatos.png')}}">
+            <img src="{{URL::asset('./image/banners/es/ceys.png')}}">
+        </div>
+        <div class="section-content">
+            <div class="d-flex justify-content-start">
+                <img id="img-section"
+                     src="{{URL::asset('/image/logo-unimar.png')}}"><span>{{trans('ceys.title')}}</span>
+            </div>
         </div>
     </div>
 @endsection
