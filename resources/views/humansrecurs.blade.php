492d0b6b8319e5395d52e298ab5de7cc290b923d
GeorgelysM <geogemar@gmail.com>
2022-03-15 21:51:57 -0400

    create humans recourses view and langs

--- resources/views/humansrecurs.blade.php
+++ resources/views/humansrecurs.blade.php
@@ -3,13 +3,13 @@
 @section ('content')
     <div class=".xl-container">
         <div class="banner-main">
-            <img src="{{URL::asset('/image/banners/es/alumni.jpg')}}">
+            <img src="{{URL::asset('/image/banners/es/rec_humans.png')}}">
         </div>
         {{--Section info--}}
         <div class="section-content">
             <div class="d-flex justify-content-start">
                 <img id="img-section"
-                     src="{{URL::asset('/image/logo-unimar.png')}}"><span>{{trans('alumni.alumni-title')}}</span>
+                     src="{{URL::asset('/image/logo-unimar.png')}}"><span>{{trans('rrhh.title')}}</span>
             </div>
         </div>
         {{--contents--}}
