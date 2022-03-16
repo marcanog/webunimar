07263a198bf2ac06fab02c9ee7a4634ba8bcad4f
GeorgelysM <geogemar@gmail.com>
2022-03-15 21:41:12 -0400

    update title section in ctrlstudies view

--- resources/views/controlstudies.blade.php
+++ resources/views/controlstudies.blade.php
@@ -8,11 +8,11 @@
         <div class="section-content">
             <div class="d-flex justify-content-start">
                 <img id="img-section"
-                     src="{{URL::asset('/image/logo-unimar.png')}}"><span>{{trans('rectorade.rector-deptos-2')}}</span>
+                     src="{{URL::asset('/image/logo-unimar.png')}}"><span>{{trans('ctrlstudies.title')}}</span>
             </div>
         </div>
         <div class="content">
-            
+
         </div>
     </div>
 @endsection
